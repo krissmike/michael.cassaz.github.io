5cf848c790d770e63a71d574e250a1cb36757e3f
krissmike <142153468+krissmike@users.noreply.github.com>
2024-01-18 14:11:51 +0000

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,13 +1,13 @@
 # Pseudocode
-# 1st I declare variables for each
+# I declare variables for each
 # I use the right variables with the right value
+# Don't declare a variable with the same name as its datatype.
+num_float = 99.23
+num_integer = 23
+num_integer1 = 150
+num_string = "100"
 
-float = 99.23
-integer = 23
-integer1 = 150
-string = "100"
-
-print(float, "into in float")
-print(integer, "into in integer")
-print(integer1, "into string integer")
-print(string, "into in string")
+print(num_float, "into in float")
+print(num_integer, "into in integer")
+print(num_integer1, "into string integer")
+print(num_string, "into in string")
